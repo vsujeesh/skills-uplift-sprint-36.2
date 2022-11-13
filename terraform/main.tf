25eb62ccdbe447e505a892bd4b6d8162e0226aa3
Vishnu Sujeesh <vsujeesh@its.jnj.com>
2022-11-14 05:20:28 +0800

    restored files

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,21 +2,14 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      Version = "~>3.27"
+      version = "~>3.27"
     }
   }
 
-  required_version = ">=0.14.9"
-#   backend "s3" {
-#        bucket = "aws-terraform-practice-vsujeesh"
-#        key    = "[Remote_State_S3_Bucket_Key]"
-#        region = "ap-southeast-1"
-#    }
 
 }
 
 provider "aws" {
-  version = "~>3.0"
   region  = "ap-southeast-1"
 }
 
@@ -26,8 +19,6 @@ resource "aws_s3_bucket" "s3Bucket" {
 
      policy  = <<EOF
 {
-     "id" : "MakePublic",
-   "version" : "2012-10-17",
    "statement" : [
       {
          "action" : [
