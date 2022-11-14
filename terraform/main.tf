34c6a75df5a41558ad709f63e9dea1cb8f382f73
Vishnu Sujeesh <vsujeesh@its.jnj.com>
2022-11-14 13:43:52 +0800

    updated terraform version

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,9 @@
 terraform {
 
-  required_version = ">=0.14.9"
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      #version = "~>3.27"
+      version = "~>3.27"
     }
   }
 
