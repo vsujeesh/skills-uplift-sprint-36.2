95816e7cc55e628017899a57a7c81b1546808b9d
Vishnu Sujeesh <vsujeesh@its.jnj.com>
2022-11-21 03:13:51 +0800

    upload index via terraform and without s3sync

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,3 +38,14 @@ resource "aws_s3_bucket" "s3" {
 
   }
 }
+
+resource "aws_s3_bucket_object" "object" {
+  bucket = "aws-terraform-practice-vsujeesh"
+  key    = "index.html"
+  source = "src/index.html"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("src/index.html")
+}
