865c31fad6cafd4b0f7df01184fad9c80d67ab71
Vishnu Sujeesh <vsujeesh@its.jnj.com>
2022-11-21 02:33:01 +0800

    added s3 backend to store state

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,23 @@
 terraform {
 
+  backend "s3" {
+    bucket = "vsujeesh-terraform-state-storage"
+    key    = "terraform.tfstate"
+    region = "ap-southeast-1"
+
+  }
+
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~>3.27"
     }
   }
 
 
 }
+
+
 
 provider "aws" {
   region  = "ap-southeast-1"
