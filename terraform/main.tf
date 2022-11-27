661651b61ddd9e9d1c6d6c334096c3a894c56503
Vishnu Sujeesh <vsujeesh@its.jnj.com>
2022-11-28 03:38:52 +0800

    basic cloudfront config with OAC policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,7 +30,7 @@ provider "aws" {
 resource "aws_s3_bucket" "s3" {
   bucket = "aws-terraform-practice-vsujeesh"
   force_destroy = "true"
-  acl    = "private"
+  acl    = "public"
   policy = file("policy.json")
 
   # website {
